a275ddb2b1295e7f89b2bad9098086c47b4ac586
Connor <connor.steele1997@hotmail.com>
2023-03-25 01:56:09 +1100

    basic_bot now restarts conversation

--- assignment2/chatbotapp/views.py
+++ assignment2/chatbotapp/views.py
@@ -8,21 +8,22 @@ from pprint import pprint
 
 class ChatForm(forms.Form):
 
-    text_area = forms.CharField(widget=forms.Textarea(attrs={'rows': '2', 'cols': '20'}), label='What would you like to do today?')
-    
+    text_area = forms.CharField(widget=forms.Textarea(
+        attrs={'rows': '2', 'cols': '20'}), label='What would you like to do today?')
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+
 class ChatBot(View):
 
     def get(self, request):
-        
+
         template = 'chatbot.html'
         posted_form = ChatForm()
-    
+
         return render(request, template, {'form': posted_form})
 
     def post(self, request):
@@ -31,22 +32,26 @@ class ChatBot(View):
         response = ''
 
         if request.method == 'POST':
-            
+
             form = ChatForm(request.POST)
             if form.is_valid():
                 text = form.cleaned_data['text_area']
+                # Using the text from the form input gather the bots apporiate slots.
                 response = lex_client.post_text(
                     botName='TravelBot',
                     botAlias='$LATEST',
                     userId='ef56a60fe85f0d59fbb82226e2f042c54f7c0e59ed4ebac602af6f8c6cda57f8',
                     inputText=text
                 )
-                if response['intentName'] == 'BookFlight':
-                    response_form = ChatForm()
-                    response_form['text_area'].label = response['message']
-            
-                    return render(request, template, {'form': response_form})
-                pprint(response)
-            
-            
 
+                # If API response returns a fulfilled dialog state, the form will restart and ask the user what they wish to do.
+                if response['dialogState'] == 'Fulfilled':
+                    restart_form = ChatForm()
+
+                    return render(request, template, {'form': restart_form})
+
+                response_form = ChatForm()
+                response_form['text_area'].label = response['message']
+
+                return render(request, template, {'form': response_form})
+            
